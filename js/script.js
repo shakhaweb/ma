4916a8cc40d5a976531a4cc90d1eceace75c752b
shakhaweb <121448974+shakhaweb@users.noreply.github.com>
2023-01-31 15:33:42 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('.btn')
+let modal_d = document.querySelector('.modal')
+
+let modal =()=>{
+    modal_d.classList.add('activ')
+}
+btn.addEventListener('click',modal)
